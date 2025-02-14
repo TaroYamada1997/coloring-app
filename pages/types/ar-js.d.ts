7a45847de1dec9616e1519bf5799cfbca198a7fb
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-02-15 02:10:08 +0900

    Update AR sample

---
+++ pages/types/ar-js.d.ts
@@ -0,0 +1,34 @@
+declare module '@ar-js-org/ar.js/three.js/build/ar-threex' {
+    export class ArToolkitSource {
+      constructor(params: { sourceType: string });
+      init(callback: () => void): void;
+      ready: boolean;
+      domElement: HTMLElement;
+      onResizeElement(): void;
+      copyElementSizeTo(element: HTMLElement): void;
+    }
+  
+    export class ArToolkitContext {
+      constructor(params: {
+        cameraParametersUrl: string;
+        detectionMode: string;
+      });
+      init(callback: () => void): void;
+      update(domElement: HTMLElement): void;
+      getProjectionMatrix(): THREE.Matrix4;
+      arController: {
+        canvas: HTMLCanvasElement;
+      } | null;
+    }
+  
+    export class ArMarkerControls {
+      constructor(
+        context: ArToolkitContext,
+        object3d: THREE.Object3D,
+        params: {
+          type: string;
+          patternUrl: string;
+        }
+      );
+    }
+  }
