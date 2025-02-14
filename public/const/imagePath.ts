630639a9fdda7551fefb7e5f7d553ea2a6f1561d
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-02-14 11:21:18 +0900

    Create Constants

---
+++ public/const/imagePath.ts
@@ -0,0 +1,14 @@
+export const coloringMap = {
+    '1': {
+      path: '/ten.png',
+      title: 'てんのぬりえ'
+    },
+    '2': {
+      path: '/ten-sample.jpg',
+      title: '実線クッキリ'
+    },
+    '3': {
+      path: '/PNG背景白あり.png',
+      title: 'サンプル'
+    }
+  } as const;
