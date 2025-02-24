d556fec5434d9699793220313ee1a6edcf4e1942
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-02-24 20:49:38 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
