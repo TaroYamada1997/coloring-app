6837a27d73990ddbe5212cdd92791e3133d844ff
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-04-02 16:44:47 +0900

    Update

---
+++ constants/Image.ts
@@ -0,0 +1,23 @@
+export const COLORINGMAP = {
+    '1': {
+      path: '/表紙イラスト.jpg',
+      title: 'JPG表紙イラスト',
+    },
+    '2': {
+      path: '/ぬりえイラスト_背景透過.png',
+      title: 'JPGぬりえイラスト',
+    },
+    '3': {
+      path: '/PNG背景白あり.png',
+      title: 'PNG背景白あり',
+    },
+    '4': {
+      path: '/PNG背景透過.png',
+      title: 'PNG背景透過',
+    },
+    '5': {
+      path: '/ten-sample背景透過.png',
+      title: 'PNG実践クッキリ背景透過',
+    },
+  } as const;
+  
