2df1860e7603a72fe39e8ee6941d82fedf7af6dd
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2025-04-09 23:46:34 +0900

    Update Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,18 +1,18 @@
 export const COLOR_CATEGORIES = {
   spring: {
     name: 'Spring',
-    colors: ['#FFB6C1', '#FFC0CB', '#FF69B4', '#FF1493', '#DB7093']
+    colors: ['#F288B9', '#F1D5E4', '#85C8F2', '#F1CE66', '#F26D6E']
   },
   summer: {
     name: 'Summer',
-    colors: ['#FF5733', '#33FFF5', '#33FF57', '#F5FF33', '#FF33F5']
+    colors: ['#91E0F1', '#63B1F3', '#F1CB06', '#F28A07', '#BF4111']
   },
   autumn: {
     name: 'Autumn',
-    colors: ['#8B4513', '#CD853F', '#D2691E', '#B8860B', '#DAA520']
+    colors: ['#465802', '#99CFF1', '#D9751F', '#731702', '#D90504']
   },
   winter: {
     name: 'Winter',
-    colors: ['#1E90FF', '#4682B4', '#87CEEB', '#B0C4DE', '#708090']
+    colors: ['#BE9A78', '#D5E7F3', '#BBCDD9', '#F2EBE5', '#BF8A84']
   },
 };
